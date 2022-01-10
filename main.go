7a6b9c55c00bbeacb25ffd8f13d5751defa355f0
Hit26 <hiteshj2612@gmail.com>
2022-01-10 15:32:33 +0530

    Updated

--- main.go
+++ main.go
@@ -1,31 +1,35 @@
 package main
 
 import (
-	"fmt"
+	"encoding/json"
 	"io/ioutil"
 	"log"
 	"net/http"
-	"os"
 
 	"github.com/gorilla/mux"
 )
 
+var res string
+
 func pResponse(w http.ResponseWriter, r *http.Request) {
 	reqBody, _ := ioutil.ReadAll(r.Body)
-	p := string(reqBody)
-	fmt.Print(p)
-	log.Print(p)
+	res = string(reqBody)
 
+}
+
+func gResponse(w http.ResponseWriter, r *http.Request) {
+	json.NewEncoder(w).Encode(res)
 }
 
 func main() {
 
-	port := os.Getenv("PORT")
+	//port := os.Getenv("PORT")
 	//Router initialization
 	r := mux.NewRouter()
 
 	//Router handler
+	r.HandleFunc("/api/responses", gResponse).Methods("GET")
 	r.HandleFunc("/api/responses", pResponse).Methods("POST")
-	log.Fatal(http.ListenAndServe(":"+port, r))
+	//log.Fatal(http.ListenAndServe(":"+port, r))
 	log.Fatal(http.ListenAndServe(":3000", r))
 }
