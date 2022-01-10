a3d52847d36c3568da74b5a00b7cdb2ba308afec
Hit26 <hiteshj2612@gmail.com>
2022-01-10 14:31:33 +0530

    first Commit

---
+++ main.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/gorilla/mux"
+)
+
+func pResponse(w http.ResponseWriter, r *http.Request) {
+	reqBody, _ := ioutil.ReadAll(r.Body)
+	fmt.Print(string(reqBody))
+
+}
+
+func main() {
+
+	port := os.Getenv("PORT")
+	//Router initialization
+	r := mux.NewRouter()
+
+	//Router handler
+	r.HandleFunc("/api/responses", pResponse).Methods("POST")
+	log.Fatal(http.ListenAndServe(port, r))
+}
